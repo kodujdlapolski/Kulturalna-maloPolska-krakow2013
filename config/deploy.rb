646d1cd742a397edf3be04f843ba51368ad2c95d
Jan Filipowski <jachuf@gmail.com>
2013-10-26 10:17:24 +0200

    Deployment - initial configuration

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+set :application, 'kulturalna-malopolska'
+set :repo_url, 'git@github.com:kodujdlapolski/Kulturalna-maloPolska.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/root/kulturalna-malopolska'
+set :scm, :git
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :default_env, { path: "/root/local/ruby-1.9.3-p448:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
