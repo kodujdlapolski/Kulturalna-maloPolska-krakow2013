9eb8d452c1b96fa97ece40db74e479ff5a2ebe28
Sylwester Grabowski <gr4b4z@gmail.com>
2013-10-26 03:52:46 +0200

    added almoast all functionality

--- frontend/app/geo.js
+++ frontend/app/geo.js
@@ -1,51 +1,55 @@
-function showLocation() 
-{
-	navigator.geolocation.getCurrentPosition(callback);
-}
- 
-function callback(position) 
-{
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude
-	codeLatLng(lat, lng).then(function(data){
-		document.getElementById('location-a').value = data; 
-	});
-	
-	var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(lat,lng),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map(document.getElementById('map-canvas'),
-		mapOptions);
-	
-}
+var geo = function () {
 
-function codeLatLng(lat, lng) 
-{
-	var address = "test";
+    var map,
+        showLocation = function () {
+                 navigator.geolocation.getCurrentPosition(callback);
+        },
+        init = function (mp) {
+            map = mp;
+        },
+    callback = function (position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude
+        codeLatLng(lat, lng).then(function (data) {
+            document.getElementById('location-a').value = data;
+        });
+
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(lat, lng),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+    }
+
+,
+codeLatLng = function (lat, lng) {
+    var address = "test";
     var latlng = new google.maps.LatLng(lat, lng);
     var defer = Q.defer();
-    
-    geocoder.geocode({'latLng': latlng}, function(results, status) 
-    {
-		if (status == google.maps.GeocoderStatus.OK) 
-		{
-			console.log(results)
-			if (results[1]) 
-			{	
-				defer.resolve(results[0].formatted_address)
-			} 
-			else 
-			{
-				alert("No results found");
-			}
-		} 
-		else 
-		{
-        alert("Geocoder failed due to: " + status);
-		}
-	});
 
-	return defer.promise;
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results)
+            if (results[1]) {
+                defer.resolve(results[0].formatted_address)
+            }
+            else {
+                alert("No results found");
+            }
+        }
+        else {
+            alert("Geocoder failed due to: " + status);
+        }
+    });
+
+    return defer.promise;
 }
+
+    return {
+        showLocation: showLocation,
+        init:init
+    }
+}();
